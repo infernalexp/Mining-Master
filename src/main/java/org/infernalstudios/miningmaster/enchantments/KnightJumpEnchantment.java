cff10cc0e5924845a010078f9afe8dc5cd330b43
HellionGames <dsgillespie02@yahoo.com>
2021-10-27 15:43:49 -0400

    Knight Jump Works

--- src/main/java/org/infernalstudios/miningmaster/enchantments/KnightJumpEnchantment.java
+++ src/main/java/org/infernalstudios/miningmaster/enchantments/KnightJumpEnchantment.java
@@ -22,7 +22,7 @@ public class KnightJumpEnchantment extends Enchantment {
 
     @Override
     public int getMaxLevel() {
-        return 1;
+        return 3;
     }
 
     @Override
--- src/main/java/org/infernalstudios/miningmaster/mixin/MixinLivingEntity.java
+++ src/main/java/org/infernalstudios/miningmaster/mixin/MixinLivingEntity.java
@@ -3,6 +3,7 @@ package org.infernalstudios.miningmaster.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.ListNBT;
@@ -20,38 +21,52 @@ public abstract class MixinLivingEntity extends Entity {
 
     @Shadow protected abstract void jump();
 
+    @Shadow public abstract ItemStack getItemStackFromSlot(EquipmentSlotType slotIn);
+
+    private int knightJumpsUsed = 0;
     private boolean hasJumped = false;
+    private int ticksSinceJump = 0;
 
     public MixinLivingEntity(EntityType<?> entityTypeIn, World worldIn) {
         super(entityTypeIn, worldIn);
     }
 
+    @Inject(method = "jump", at = @At(value = "HEAD"))
+    private void MM_setHasJumped(CallbackInfo ci) {
+        ticksSinceJump = 0;
+        this.hasJumped = true;
+    }
+
     @Inject(method = "livingTick", at = @At(value = "HEAD"))
     private void MM_countTicksFalling(CallbackInfo ci) {
         if (this.onGround) {
+            this.ticksSinceJump = 0;
             this.hasJumped = false;
+            this.knightJumpsUsed = 0;
+        } else {
+            this.ticksSinceJump++;
         }
     }
 
     @Inject(method = "livingTick", at = @At(target = "Lnet/minecraft/entity/LivingEntity;getFluidJumpHeight()D", value = "INVOKE", shift = At.Shift.AFTER))
     private void MM_knightJump(CallbackInfo ci) {
-        boolean hasKnightJump = false;
+        if (!this.onGround) {
+            boolean hasKnightJump = false;
+            int level = 0;
 
-        if (!this.hasJumped) {
-            for (ItemStack stack: this.getHeldEquipment()) {
-                ListNBT nbtList = stack.getEnchantmentTagList();
+            ItemStack stack = this.getItemStackFromSlot(EquipmentSlotType.LEGS);
+            ListNBT nbtList = stack.getEnchantmentTagList();
 
-                for (int i = 0; i < nbtList.size(); i++) {
-                    CompoundNBT idTag = nbtList.getCompound(i);
-
-                    if (idTag.getString("id").equals(MMEnchantments.KNIGHT_JUMP.getId().toString())) {
-                        hasKnightJump = true;
-                    }
+            for (int i = 0; i < nbtList.size(); i++) {
+                CompoundNBT idTag = nbtList.getCompound(i);
+                if (idTag.getString("id").equals(MMEnchantments.KNIGHT_JUMP.getId().toString())) {
+                    hasKnightJump = true;
+                    level = idTag.getInt("lvl");
                 }
             }
 
-            if (hasKnightJump) {
-                this.hasJumped = true;
+            if (this.knightJumpsUsed < level && hasKnightJump && (!this.hasJumped || ticksSinceJump >= 5)) {
+                this.knightJumpsUsed++;
                 this.jump();
                 this.jumpTicks = 10;
             }
