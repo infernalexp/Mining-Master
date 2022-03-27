d20b718c07168d556cbb99af7c6fef3edbcbd325
HellionGames <dsgillespie02@yahoo.com>
2022-03-27 12:24:19 -0400

    Switched out forge recipe book gui, fixed ghost recipe display

--- src/main/java/org/infernalstudios/miningmaster/client/gui/recipebook/GemForgeRecipeGui.java
+++ src/main/java/org/infernalstudios/miningmaster/client/gui/recipebook/GemForgeRecipeGui.java
@@ -16,10 +16,18 @@
 
 package org.infernalstudios.miningmaster.client.gui.recipebook;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.IGuiEventListener;
+import net.minecraft.client.gui.IRenderable;
+import net.minecraft.client.gui.recipebook.IRecipeUpdateListener;
 import net.minecraft.client.gui.recipebook.RecipeBookGui;
+import net.minecraft.client.gui.recipebook.RecipeTabToggleWidget;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipePlacer;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -28,13 +36,25 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import org.infernalstudios.miningmaster.MiningMaster;
 
 import java.util.Iterator;
+import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
-public class GemForgeRecipeGui extends RecipeBookGui {
+public class GemForgeRecipeGui extends RecipeBookGui implements IRenderable, IGuiEventListener, IRecipeUpdateListener, IRecipePlacer<Ingredient> {
+    protected static final ResourceLocation RECIPE_BOOK_GEM_FORGE = new ResourceLocation(MiningMaster.MOD_ID, "textures/gui/recipe_book_gem_forge.png");
     private static final ITextComponent translationKeyForgable = new TranslationTextComponent(MiningMaster.MOD_ID + ".gui.recipebook.toggleRecipes.forgable");
 
     protected ITextComponent func_230479_g_() {
         return translationKeyForgable;
+    }
+
+    protected void func_205702_a() {
+        this.toggleRecipesBtn.initTextureValues(152, 41, 28, 18, RECIPE_BOOK_GEM_FORGE);
+    }
+
+    @Override
+    public void setupGhostRecipe(IRecipe<?> recipe, List<Slot> slots) {
+        this.ghostRecipe.setRecipe(recipe);
+        this.placeRecipe(this.field_201522_g.getWidth(), this.field_201522_g.getHeight(), this.field_201522_g.getOutputSlot(), recipe, recipe.getIngredients().iterator(), 0);
     }
 
     @Override
@@ -45,7 +65,8 @@ public class GemForgeRecipeGui extends RecipeBookGui {
 
             if (ingredients.hasNext()) {
                 slot = this.field_201522_g.inventorySlots.get(slotIn);
-            } else {
+            }
+            else {
                 slot = this.field_201522_g.inventorySlots.get(this.field_201522_g.getOutputSlot());
             }
 
@@ -94,5 +115,36 @@ public class GemForgeRecipeGui extends RecipeBookGui {
             }
         }
 
+    }
+
+    @Override
+    public void func_230477_a_(MatrixStack matrixStack, int xOffset, int yOffset, boolean displayOutputSquare, float time) {
+        super.func_230477_a_(matrixStack, xOffset, yOffset, false, time);
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        if (this.isVisible()) {
+            RenderSystem.pushMatrix();
+            RenderSystem.translatef(0.0F, 0.0F, 100.0F);
+            this.mc.getTextureManager().bindTexture(RECIPE_BOOK_GEM_FORGE);
+            RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+            int i = (this.width - 147) / 2 - this.xOffset;
+            int j = (this.height - 166) / 2;
+            this.blit(matrixStack, i, j, 1, 1, 147, 166);
+            if (!this.searchBar.isFocused() && this.searchBar.getText().isEmpty()) {
+                drawString(matrixStack, this.mc.fontRenderer, field_241620_l_, i + 25, j + 14, -1);
+            } else {
+                this.searchBar.render(matrixStack, mouseX, mouseY, partialTicks);
+            }
+
+            for(RecipeTabToggleWidget recipetabtogglewidget : this.recipeTabs) {
+                recipetabtogglewidget.render(matrixStack, mouseX, mouseY, partialTicks);
+            }
+
+            this.toggleRecipesBtn.render(matrixStack, mouseX, mouseY, partialTicks);
+            this.recipeBookPage.func_238927_a_(matrixStack, i, j, mouseX, mouseY, partialTicks);
+            RenderSystem.popMatrix();
+        }
     }
 }
