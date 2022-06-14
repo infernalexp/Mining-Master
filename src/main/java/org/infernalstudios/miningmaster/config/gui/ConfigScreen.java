7d67166dd956ffd3269cc8cac41bdab2545a4ade
HellionGames <dsgillespie02@yahoo.com>
2022-06-13 20:42:26 -0400

    Began porting config GUI

--- src/main/java/org/infernalstudios/miningmaster/config/gui/ConfigScreen.java
+++ src/main/java/org/infernalstudios/miningmaster/config/gui/ConfigScreen.java
@@ -17,15 +17,13 @@
 package org.infernalstudios.miningmaster.config.gui;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.CycleOption;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.ProgressOption;
+import net.minecraft.client.OptionInstance;
+import net.minecraft.client.Options;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.OptionsList;
 import net.minecraft.client.gui.screens.OptionsSubScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.util.FormattedCharSequence;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -41,7 +39,7 @@ public class ConfigScreen extends Screen {
     private OptionsList optionsRowList;
 
     public ConfigScreen() {
-        super(new TranslatableComponent(MiningMaster.MOD_ID + ".config.title"));
+        super(Component.translatable(MiningMaster.MOD_ID + ".config.title"));
     }
 
     @SuppressWarnings("resource")
@@ -50,89 +48,89 @@ public class ConfigScreen extends Screen {
         optionsRowList = new OptionsList(minecraft, width, height, 24, height - 32, 25);
 
         // Gem Booleans
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.fireRubyEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.fireRubyEnabled"),
-                settings -> MiningMasterConfig.CONFIG.fireRubyEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.fireRubyEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.fireRubyEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.fireRubyEnabled")),
+                MiningMasterConfig.CONFIG.fireRubyEnabled.get(), MiningMasterConfig.CONFIG.fireRubyEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.iceSapphireEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.iceSapphireEnabled"),
-                settings -> MiningMasterConfig.CONFIG.iceSapphireEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.iceSapphireEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.iceSapphireEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.iceSapphireEnabled")),
+                MiningMasterConfig.CONFIG.iceSapphireEnabled.get(), MiningMasterConfig.CONFIG.iceSapphireEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.spiritGarnetEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.spiritGarnetEnabled"),
-                settings -> MiningMasterConfig.CONFIG.spiritGarnetEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.spiritGarnetEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.spiritGarnetEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.spiritGarnetEnabled")),
+                MiningMasterConfig.CONFIG.spiritGarnetEnabled.get(), MiningMasterConfig.CONFIG.spiritGarnetEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.hastePeridotEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.hastePeridotEnabled"),
-                settings -> MiningMasterConfig.CONFIG.hastePeridotEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.hastePeridotEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.hastePeridotEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.hastePeridotEnabled")),
+                MiningMasterConfig.CONFIG.hastePeridotEnabled.get(), MiningMasterConfig.CONFIG.hastePeridotEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.luckyCitrineEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.luckyCitrineEnabled"),
-                settings -> MiningMasterConfig.CONFIG.luckyCitrineEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.luckyCitrineEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.luckyCitrineEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.luckyCitrineEnabled")),
+                MiningMasterConfig.CONFIG.luckyCitrineEnabled.get(), MiningMasterConfig.CONFIG.luckyCitrineEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.diveAquamarineEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.diveAquamarineEnabled"),
-                settings -> MiningMasterConfig.CONFIG.diveAquamarineEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.diveAquamarineEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.diveAquamarineEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.diveAquamarineEnabled")),
+                MiningMasterConfig.CONFIG.diveAquamarineEnabled.get(), MiningMasterConfig.CONFIG.diveAquamarineEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.heartRhodoniteEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.heartRhodoniteEnabled"),
-                settings -> MiningMasterConfig.CONFIG.heartRhodoniteEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.heartRhodoniteEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.heartRhodoniteEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.heartRhodoniteEnabled")),
+                MiningMasterConfig.CONFIG.heartRhodoniteEnabled.get(), MiningMasterConfig.CONFIG.heartRhodoniteEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.powerPyriteEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.powerPyriteEnabled"),
-                settings -> MiningMasterConfig.CONFIG.powerPyriteEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.powerPyriteEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.powerPyriteEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.powerPyriteEnabled")),
+                MiningMasterConfig.CONFIG.powerPyriteEnabled.get(), MiningMasterConfig.CONFIG.powerPyriteEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.kineticOpalEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.kineticOpalEnabled"),
-                settings -> MiningMasterConfig.CONFIG.kineticOpalEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.kineticOpalEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.kineticOpalEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.kineticOpalEnabled")),
+                MiningMasterConfig.CONFIG.kineticOpalEnabled.get(), MiningMasterConfig.CONFIG.kineticOpalEnabled::set
         ));
 
-        optionsRowList.addBig(CycleOption.createOnOff(MiningMaster.MOD_ID + ".config.option.airMalachiteEnabled",
-                new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.airMalachiteEnabled"),
-                settings -> MiningMasterConfig.CONFIG.airMalachiteEnabled.get(), (settings, option, value) -> MiningMasterConfig.CONFIG.airMalachiteEnabled.set(value)
+        optionsRowList.addBig(OptionInstance.createBoolean(MiningMaster.MOD_ID + ".config.option.airMalachiteEnabled",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.airMalachiteEnabled")),
+                MiningMasterConfig.CONFIG.airMalachiteEnabled.get(), MiningMasterConfig.CONFIG.airMalachiteEnabled::set
         ));
 
         // Common Gem Spawns
-        optionsRowList.addBig(new ProgressOption(MiningMaster.MOD_ID + ".config.option.commonGemsPerChunk", 0, 100, 1,
-                settings -> MiningMasterConfig.CONFIG.commonGemsPerChunk.get().doubleValue(), (settings, value) -> MiningMasterConfig.CONFIG.commonGemsPerChunk.set(value.intValue()),
-                (settings, option) -> new TranslatableComponent("options.generic_value", option.getCaption(), // getBaseMessageTranslation() is protected by default, use an access transformer to be able to use it
-                        new TextComponent(Double.toString((double) Math.round(option.get(settings) * 100) / 100))),
-                (minecraft) -> Minecraft.getInstance().font.split(
-                        new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.commonGemsPerChunk"), 200)
+        optionsRowList.addBig(new OptionInstance<>(MiningMaster.MOD_ID + ".config.option.commonGemsPerChunk",
+                OptionInstance.cachedConstantTooltip(Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.commonGemsPerChunk")),
+                Options::genericValueLabel, new OptionInstance.IntRange(0, 100),
+                MiningMasterConfig.CONFIG.commonGemsPerChunk.get(), MiningMasterConfig.CONFIG.commonGemsPerChunk::set
                 )
         );
+/*
 
         // Rare Gem Spawns
-        optionsRowList.addBig(new ProgressOption(MiningMaster.MOD_ID + ".config.option.rareGemsPerChunk", 0, 100, 1,
+        optionsRowList.addBig(new OptionInstance<Integer>(MiningMaster.MOD_ID + ".config.option.rareGemsPerChunk", 0, 100, 1,
                 settings -> MiningMasterConfig.CONFIG.rareGemsPerChunk.get().doubleValue(), (settings, value) -> MiningMasterConfig.CONFIG.rareGemsPerChunk.set(value.intValue()),
-                (settings, option) -> new TranslatableComponent("options.generic_value", option.getCaption(), // getBaseMessageTranslation() is protected by default, use an access transformer to be able to use it
-                        new TextComponent(Double.toString((double) Math.round(option.get(settings) * 100) / 100))),
+                (settings, option) -> Component.translatable("options.generic_value", option.getCaption(), // getBaseMessageTranslation() is protected by default, use an access transformer to be able to use it
+                        Component.literal(Double.toString((double) Math.round(option.get(settings) * 100) / 100))),
                 (minecraft) -> Minecraft.getInstance().font.split(
-                        new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.rareGemsPerChunk"), 200)
+                        Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.rareGemsPerChunk"), 200)
                 )
         );
 
         // Random Gem Spawns
-        optionsRowList.addBig(new ProgressOption(MiningMaster.MOD_ID + ".config.option.randomGemsPerChunk", 0, 100, 1,
+        optionsRowList.addBig(new OptionInstance<Integer>(MiningMaster.MOD_ID + ".config.option.randomGemsPerChunk", 0, 100, 1,
                 settings -> MiningMasterConfig.CONFIG.randomGemsPerChunk.get().doubleValue(), (settings, value) -> MiningMasterConfig.CONFIG.randomGemsPerChunk.set(value.intValue()),
-                (settings, option) -> new TranslatableComponent("options.generic_value", option.getCaption(), // getBaseMessageTranslation() is protected by default, use an access transformer to be able to use it
-                        new TextComponent(Double.toString((double) Math.round(option.get(settings) * 100) / 100))),
+                (settings, option) -> Component.translatable("options.generic_value", option.getCaption(), // getBaseMessageTranslation() is protected by default, use an access transformer to be able to use it
+                        Component.literal(Double.toString((double) Math.round(option.get(settings) * 100) / 100))),
                 (minecraft) -> Minecraft.getInstance().font.split(
-                        new TranslatableComponent(MiningMaster.MOD_ID + ".config.tooltip.randomGemsPerChunk"), 200)
+                        Component.translatable(MiningMaster.MOD_ID + ".config.tooltip.randomGemsPerChunk"), 200)
                 )
         );
+*/
 
         addWidget(optionsRowList);
 
-        addWidget(new Button((width - 200) / 2, height - 26, 200, 20, new TranslatableComponent("gui.done"), button -> onClose()));
+        addWidget(new Button((width - 200) / 2, height - 26, 200, 20, Component.translatable("gui.done"), button -> onClose()));
     }
 
     @Override
